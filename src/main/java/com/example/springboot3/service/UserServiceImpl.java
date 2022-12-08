2e38b019d3da6120a87e969a043f5095e00cf537
Andrey <andry.ivankov@ya.ru>
2022-12-08 21:10:27 +0300

    .

--- src/main/java/com/example/springboot3/service/UserServiceImpl.java
+++ src/main/java/com/example/springboot3/service/UserServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+
 import com.example.springboot3.entity.User;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,38 +25,45 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         this.userDao = userDao;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
     @Override
     @Transactional
     public void addUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userDao.addUser(user);
     }
+
     @Override
     @Transactional
     public void updateUser(User user) {
-        if (user.getPassword().equals(getUserById(user.getId()).getPassword())) {
-            user.setPassword(getUserById(user.getId()).getPassword());
-        } else {
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        }
+        user.setPassword(getUserById(user.getId()).getPassword());
         userDao.updateUser(user);
     }
+
     @Override
     @Transactional
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
+
     @Override
     @Transactional
-    public User getUserById(long id) { return userDao.getUserById(id); }
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
     @Override
     @Transactional(readOnly = true)
-    public List<User> getAllUsers() { return userDao.getAllUsers(); }
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
     @Override
     @Transactional
     public User getUserByName(String username) {
         return userDao.getUserByName(username);
     }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = getUserByName(s);
